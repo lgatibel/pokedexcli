a72677c34a337ccbdcacf3efe0c1281feeb17903
Lionel Gatibelza <lionelgatibelza@gmail.com>
2025-04-28 23:39:46 +0200

    add repl prompt and basic command parssing

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	return strings.Split(strings.Trim(strings.ToLower(text), " "), " ")
+}
 
 func main() {
-  fmt.Println("Hello, World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	prompt := "Pokedex > "
+	baseMesage := "Your command was: %s\n"
+	fmt.Printf("%s", prompt)
+	for scanner.Scan() {
+		text := scanner.Text()
+		words := cleanInput(text)
+		firstWord := words[0]
+		fmt.Printf(baseMesage, firstWord)
+		fmt.Printf("%s", prompt)
+	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "shouldn't see an error scanning a string")
+	}
 }
